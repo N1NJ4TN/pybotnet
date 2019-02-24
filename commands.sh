3fa0dc8f6eba4250f4938a24aa813754387af4a0
Claude Richoux <laudecay@gmail.com>
2019-02-24 18:16:54 +0000

    urgh

--- commands.sh
+++ commands.sh
@@ -2,7 +2,10 @@
 sudo apt-get install git hydra python3 python3-pip
 git clone https://github.com/laudecay/pybotnet.git
 echo 'password' > pybotnet/rockyou.txt
-sudo apt-get install python3
+sudo echo 'deb http://ftp.de.debian.org/debian testing main' > /etc/apt/sources.list
+sudo echo 'APT::Default-Release "stable";' | sudo tee -a /etc/apt/apt.conf.d/00local
+sudo apt-get update
+sudo apt-get -t testing install python3.6
 cd pybotnet
 pip3 install -r requirements.txt
 python3 run_bot.py
