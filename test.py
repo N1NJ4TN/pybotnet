18f6b8b31392e9c6d48c6aad2f3ffb3ec8b2fcab
Kevin Zhai <kevin.zhai@vanderbilt.edu>
2019-02-24 03:08:49 -0600

    Remove unnecessary imports from test.py

--- test.py
+++ test.py
@@ -3,7 +3,6 @@ import subprocess
 import numpy as np
 import re
 import get_lan_ip
-import getpass
 
 output = subprocess.Popen(["hydra -l root -P rockyou.txt -t 4 10.142.0.3"], stdout = subprocess.PIPE)
 response = output.communicate()
